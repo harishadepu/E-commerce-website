fb48114fd75a9693fd2c17605b47637b257c3fb7
Harish Adepu <harishadepu40@gmail.com>
2025-07-11 13:19:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(express.json());
 const cors = require('cors');
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3001"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
 
 app.use(cors({
   origin: function (origin, callback) {
